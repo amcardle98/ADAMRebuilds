1c19ce2175ad8961ea47ad9d4548afa8b7bb1706
Aadam Zocolo <azocolo@gmail.com>
2022-05-09 17:07:13 -0400

    Loading indicator while loading auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,39 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
 import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { filter, take } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('sidenavContainer')
   sidenavContainerComponent: MatSidenavContainer;
 
   @ViewChild('sidenav')
   sidenavComponent: MatSidenav;
 
-  constructor(private readonly router: Router) {}
+  authenticationLoaded = false;
+
+  constructor(
+    private readonly router: Router,
+    private readonly authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authService
+      .WatchCurrentUser()
+      .pipe(untilDestroyed(this), take(1))
+      .subscribe((u) => {
+        console.log('Loaded auth with user: ', u);
+        this.authenticationLoaded = true;
+      });
+  }
 
   ngAfterViewInit(): void {
     this.router.events
